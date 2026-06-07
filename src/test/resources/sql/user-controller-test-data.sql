548074ced6fd7060992525b10e3fa9bddac68055
gihyungcoding <gihyung.coding@gmail.com>
2026-06-07 20:00:43 +0900

    add : 컨트롤러 테스트 추가 - UserControllerTest, HealthCheckTest

---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'gihyung.coding@gmail.com', 'gihyunglee', 'Seoul', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaa', 'ACTIVE', 0),
+       (2, 'gihyung.coding2@gmail.com', 'gihyunglee2', 'Seoul', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaa', 'PENDING', 0),
+       (3, 'gihyung.coding3@gmail.com', 'gihyunglee3', 'Seoul', 'aaaaaa-aaaa-aaaa-aaaa-aaaaaa', 'INACTIVE', 0);
