b3c486fcb7e272f9de56ecdb7817c3e6f028e3b6
gihyungcoding <gihyung.coding@gmail.com>
2026-06-08 22:44:45 +0900

    refactor: 인증 서비스 분리, mailsender 의존성 역전

---
+++ src/main/java/com/example/demo/user/infrastructure/MailSenderImpl.java
@@ -0,0 +1,23 @@
+package com.example.demo.user.infrastructure;
+
+import com.example.demo.user.service.port.MailSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MailSenderImpl implements MailSender {
+
+    private final JavaMailSender mailSender;
+
+    @Override
+    public void send(String email, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject(subject);
+        message.setText(content);
+        mailSender.send(message);
+    }
+}
